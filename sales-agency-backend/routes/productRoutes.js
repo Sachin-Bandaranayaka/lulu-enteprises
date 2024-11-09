bf318082de08ad5121f28bb970353b233769c587
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-11-09 18:18:24 +0530

    Update productRoutes.js

--- sales-agency-backend/routes/productRoutes.js
+++ sales-agency-backend/routes/productRoutes.js
@@ -6,6 +6,7 @@ const productController = require('../controllers/productController');
 router.get('/', productController.getProducts);
 router.post('/', productController.addProduct);
 router.put('/:id', productController.updateProduct);
+router.delete('/:id', productController.deleteProduct);
 // Add more routes as needed
 
 module.exports = router;
