aaa89958d9f3a720b345b9f62c2b5888e6be6fb4
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-11-15 01:58:57 +0530

    Update invoiceRoutes.js

--- sales-agency-backend/routes/invoiceRoutes.js
+++ sales-agency-backend/routes/invoiceRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const invoiceController = require('../controllers/invoiceController');
 
 router.post('/', invoiceController.createInvoice);
+router.delete('/lastInvoice', invoiceController.deleteLastInvoice);
 // Add more routes as needed
 
 router.get('/lastInvoice', invoiceController.getLastInvoice);
