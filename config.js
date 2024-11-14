495bce686556e2384db83861f7c7160a94b1a629
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-11-15 01:59:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,6 +23,7 @@ export const PRODUCTS_API = `${BASE_URL}/api/products`;
 export const INVOICES_API = `${BASE_URL}/api/invoices`;
 export const EXPENSES_API = `${BASE_URL}/api/expenses`;
 export const DISCOUNT_RULES_API = `${BASE_URL}/api/discountRules`;
+export const STOCK_API = `${BASE_URL}/api/stock`;
 
 export default BASE_URL;
 
